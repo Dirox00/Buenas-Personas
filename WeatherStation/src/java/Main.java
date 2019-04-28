0a1764416fb3c17a826338641d4fb07263bf1ba4
Escuaralininge <36053817+Escuaralininge@users.noreply.github.com>
2019-04-28 19:38:45 +0200

    Add files via upload

---
+++ WeatherStation/src/java/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class Main {
+
+	public static void main (String [] args)  {
+
+		System.out.println("Hola, Mundo!");
+
+	}
+
+}
